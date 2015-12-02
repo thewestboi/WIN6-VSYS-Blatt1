dc3732177b8c754dfbc43d980a8ced403ed284b4
hlk65 <luhansen@htwg-konstanz.de>
2015-12-02 15:42:53 +0100

    B2 A1b done

--- src/blatt2Aufgabe1b/ClientMain.java
+++ src/blatt2Aufgabe1b/ClientMain.java
@@ -6,17 +6,19 @@ import java.io.InputStreamReader;
 public class ClientMain {
 	private static final int port=1234;
 	private static final String hostname="localhost";
-	private static MySocketClient client;
 	private static BufferedReader reader;
 
 	public static void main(String args[]) {
 		try {
-			client=new MySocketClient(hostname,port);
 			System.out.print("Client: Enter name> ");
 			reader=new BufferedReader(new InputStreamReader(System.in ));
 			String clientName=reader.readLine();
-			System.out.println(client.sendAndReceive(clientName));
-			client.disconnect();
+			MyThread myThread = new MyThread(hostname, port, clientName);
+			myThread.start();
+			reader.readLine();
+			myThread.setStop(true);
+			System.out.println("Stopped");
+			
 		}
 		catch(Exception e) {
 			e.printStackTrace();
